ba5d14983ac95cb76fb3e30c5c031eca427a63e6
nikolalsvk <nikolaseap@gmail.com>
2017-04-25 11:38:54 +0200

    Style calendar screen

--- App/Containers/CalendarScreen.js
+++ App/Containers/CalendarScreen.js
@@ -1,19 +1,24 @@
 import React from 'react'
 import { View } from 'react-native'
 import { Images } from '../Themes'
+import ListviewExample from './ListviewExample.js'
+
+// Styles
+import styles from './Styles/CalendarScreenStyles'
+import listStyles from './Styles/ListviewExampleStyles'
 
 import CalendarStrip from 'react-native-calendar-strip'
 
 class CalendarScreen extends React.Component {
   render () {
     return (
-      <View>
+      <View style={listStyles.container}>
         <CalendarStrip
           calendarAnimation={{type: 'sequence', duration: 30}}
           daySelectionAnimation={{type: 'border',
             duration: 300,
-            borderWidth: 2,
-            borderHighlightColor: 'white'}}
+            borderWidth: 1,
+            borderHighlightColor: 'red'}}
           style={{height: 100, paddingTop: 20, paddingBottom: 10}}
           calendarHeaderStyle={{color: 'white'}}
           calendarColor={'#7743CE'}
@@ -21,8 +26,11 @@ class CalendarScreen extends React.Component {
           dateNameStyle={{color: 'white'}}
           iconLeft={Images.leftArrow}
           iconRight={Images.rightArrow}
-          iconContainer={{flex: 0.2}}
+          iconContainer={{flex: 0}}
         />
+        <View style={listStyles.container}>
+          <ListviewExample />
+        </View>
       </View>
     )
   }
