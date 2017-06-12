98b7a15c425aef88ff5900ce61e22ab24b60156c
nikolalsvk <nikolaseap@gmail.com>
2017-06-13 01:08:04 +0200

    Change LoginRedux to set userId instead of username

--- App/Redux/LoginRedux.js
+++ App/Redux/LoginRedux.js
@@ -5,7 +5,7 @@ import Immutable from 'seamless-immutable'
 
 const { Types, Creators } = createActions({
   loginRequest: ['username', 'password'],
-  loginSuccess: ['username'],
+  loginSuccess: ['userId'],
   loginFailure: ['error'],
   logout: null
 })
@@ -16,7 +16,7 @@ export default Creators
 /* ------------- Initial State ------------- */
 
 export const INITIAL_STATE = Immutable({
-  username: null,
+  userId: null,
   error: null,
   fetching: false
 })
@@ -27,8 +27,8 @@ export const INITIAL_STATE = Immutable({
 export const request = (state) => state.merge({ fetching: true })
 
 // we've successfully logged in
-export const success = (state, { username }) =>
-  state.merge({ fetching: false, error: null, username })
+export const success = (state, { userId }) =>
+  state.merge({ fetching: false, error: null, userId })
 
 // we've had a problem logging in
 export const failure = (state, { error }) =>
@@ -49,4 +49,4 @@ export const reducer = createReducer(INITIAL_STATE, {
 /* ------------- Selectors ------------- */
 
 // Is the current user logged in?
-export const isLoggedIn = (loginState) => loginState.username !== null
+export const isLoggedIn = (loginState) => loginState.userId !== null
