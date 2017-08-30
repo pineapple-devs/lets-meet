0b0da218f801902fef0efbead2a08b5d79ed9899
nikolalsvk <nikolaseap@gmail.com>
2017-08-30 23:43:32 +0200

    Format NewMeetingForm

--- App/Components/NewMeetingForm.js
+++ App/Components/NewMeetingForm.js
@@ -1,27 +1,37 @@
 import React, {Component} from 'react';
 
-import {StyleSheet, Text, View, ScrollView, TouchableOpacity, TouchableHighlight} from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  ScrollView,
+  TouchableOpacity,
+  TouchableHighlight,
+} from 'react-native';
 import Icon from 'react-native-vector-icons/Ionicons';
 import styles from './Styles/RectangleButtonStyles';
 import {DatePicker} from 'react-native-ui-xg';
 
-
-
-import { Form,
-  Separator,InputField, LinkField,
-  SwitchField, PickerField,DatePickerField
+import {
+  Form,
+  Separator,
+  InputField,
+  LinkField,
+  SwitchField,
+  PickerField,
+  DatePickerField,
 } from 'react-native-form-generator';
 
-export class NewMeetingForm extends React.Component{
-  constructor(props){
+export class NewMeetingForm extends React.Component {
+  constructor(props) {
     super(props);
     this.state = {
-      formData:{},
-      showModal: false
-    }
+      formData: {},
+      showModal: false,
+    };
   }
 
-  handleFormChange(formData){
+  handleFormChange(formData) {
     /*
     formData will contain all the values of the form,
     in this example.
@@ -35,87 +45,92 @@ export class NewMeetingForm extends React.Component{
     }
     */
 
-    this.setState({formData:formData})
+    this.setState({formData: formData});
     this.props.onFormChange && this.props.onFormChange(formData);
   }
 
-  handleFormFocus(e, component){
-    //console.log(e, component);
+  handleFormFocus(e, component) {
+    // console.log(e, component);
   }
 
   handleSubmit(formData) {
-    this.setState({formData:formData})
+    this.setState({formData: formData});
   }
 
-  openTermsAndConditionsURL(){
+  openTermsAndConditionsURL() {}
+  render() {
+    return (
+      <ScrollView
+        keyboardShouldPersistTaps="always"
+        style={{paddingLeft: 10, paddingRight: 10, height: 200}}>
+        <Form
+          ref="addNewMeetingForm"
+          onFocus={this.handleFormFocus.bind(this)}
+          onChange={this.handleFormChange.bind(this)}
+          onSubmit={formData => onSubmit(formData)}
+          label="Personal Information">
+          <Separator />
 
-  }
-  render(){
-    return (<ScrollView keyboardShouldPersistTaps='always' style={{paddingLeft:10,paddingRight:10, height:200}}>
-      <Form
-        ref='addNewMeetingForm'
-        onFocus={this.handleFormFocus.bind(this)}
-        onChange={this.handleFormChange.bind(this)}
-        onSubmit={formData => onSubmit(formData)}
-        label="Personal Information">
-        <Separator />
+          <InputField ref="meeting_name" placeholder="Meeting name" />
 
-        <InputField ref='meeting_name' placeholder='Meeting name'/>
+          <InputField
+            multiline={true}
+            ref="meeting_description"
+            placeholder="Description"
+            helpText="Write down some special reminders for your meeting!"
+          />
+          <Separator />
 
-        <InputField
-          multiline={true}
-          ref='meeting_description'
-          placeholder='Description'
-          helpText='Write down some special reminders for your meeting!' />
-        <Separator />
+          <SwitchField
+            label="Repeat"
+            ref="wants_reminder"
+            helpText="Check if you want your meeting to repeat."
+          />
 
-        <SwitchField label='Repeat'
-          ref="wants_reminder"
-          helpText='Check if you want your meeting to repeat.'/>
+          <PickerField
+            ref="gender"
+            label="Repeat settings"
+            options={{
+              '': '',
+              every_day: 'Every day',
+              every_week: 'Every week',
+              every_month: 'Every month',
+            }}
+          />
 
-        <PickerField ref='gender'
-          label='Repeat settings'
-          options={{
-            "": '',
-            every_day: 'Every day',
-            every_week: 'Every week',
-            every_month: 'Every month'
-          }}/>
+          <DatePickerField
+            ref="meeting_date1"
+            minimumDate={new Date('1/1/1900')}
+            maximumDate={new Date()}
+            placeholder="Date"
+          />
 
-          <DatePickerField ref='meeting_date1'
-          minimumDate={new Date('1/1/1900')}
-          maximumDate={new Date()}
-          placeholder='Date'/>
-
-                <DatePicker
-                  style={{width: 200}}
-                  mode="datetime"
-                  format="YYYY-MM-DD HH:mm"
-                  confirmBtnText="Confirm"
-                  cancelBtnText="Cancel"
-                  ref = "date_and_time"
-                  customStyles={{
-                    dateIcon: {
-                      position: 'absolute',
-                      left: 0,
-                      top: 4,
-                      marginLeft: 0
-                    },
-                    dateInput: {
-                      marginLeft: 36
-                    }
-                  }}
-                  minuteInterval={10}
-                />
-
-
-
+          <DatePicker
+            style={{width: 200}}
+            mode="datetime"
+            format="YYYY-MM-DD HH:mm"
+            confirmBtnText="Confirm"
+            cancelBtnText="Cancel"
+            ref="date_and_time"
+            customStyles={{
+              dateIcon: {
+                position: 'absolute',
+                left: 0,
+                top: 4,
+                marginLeft: 0,
+              },
+              dateInput: {
+                marginLeft: 36,
+              },
+            }}
+            minuteInterval={10}
+          />
         </Form>
-         <Separator/>
+        <Separator />
 
         <Text>{JSON.stringify(this.state.formData)}</Text>
-
-      </ScrollView>);
-    }
+      </ScrollView>
+    );
   }
-export default NewMeetingForm
+}
+export default NewMeetingForm;
