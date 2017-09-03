ad1ad9e0ec45add395438f1ea4dc1dc9bea21a14
Jana Vojnovic <jana_vojnovic@hotmail.com>
2017-09-03 22:03:44 +0200

    Trying out the modal window

--- App/Containers/MeetingDetailsScreen.js
+++ App/Containers/MeetingDetailsScreen.js
@@ -1,5 +1,13 @@
 import React from "react";
-import { ScrollView, Text, Image, View, StyleSheet } from "react-native";
+import {
+  ScrollView,
+  Text,
+  Image,
+  View,
+  StyleSheet,
+  Modal,
+  TouchableHighlight
+} from "react-native";
 import CalendarButton from "../Containers/Buttons/CalendarButton.js";
 import { Actions } from "react-native-router-flux";
 import Icon from "react-native-vector-icons/FontAwesome";
@@ -14,11 +22,49 @@ export default class LaunchScreen extends React.Component {
     super(props);
   }
 
+  state = {
+    modalVisible: false
+  };
+
+  setModalVisible(visible) {
+    this.setState({ modalVisible: visible });
+  }
+
   render() {
     return (
       <ScrollView keyboardShouldPersistTaps="always" style={styles.mainView}>
         <Text style={styles.text}>{this.props.meetingData.title}</Text>
         <Text style={styles.text}>{this.props.meetingData.description}</Text>
+
+        <Modal
+          animationType="slide"
+          transparent={false}
+          visible={this.state.modalVisible}
+          onRequestClose={() => {
+            alert("Modal has been closed.");
+          }}
+        >
+          <View style={{ marginTop: 22 }}>
+            <View>
+              <Text>Hello World!</Text>
+
+              <TouchableHighlight
+                onPress={() => {
+                  this.setModalVisible(!this.state.modalVisible);
+                }}
+              >
+                <Text>Hide Modal</Text>
+              </TouchableHighlight>
+            </View>
+          </View>
+        </Modal>
+        <TouchableHighlight
+          onPress={() => {
+            this.setModalVisible(true);
+          }}
+        >
+          <Text>Show Modal</Text>
+        </TouchableHighlight>
       </ScrollView>
     );
   }
